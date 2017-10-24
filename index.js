80524ef52db03d6428018c5d19fa1d149558fe83
Joseph Papaluca <jpapaluc@gmail.com>
2017-10-24 14:56:30 -0400

    finished rough responsive layout design, need to add content

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ $(document).ready(function() {
 
   $('.test').on('click', function() {
     $('.modal').fadeIn('slow');
+    $('body').addClass('stop-scrolling');
   });
 
   $('.modal').on('click', function() {
-    $('iframe').attr('src', $('iframe').attr('src')); 
+    $('iframe').attr('src', $('iframe').attr('src'));
     $('.modal').fadeOut('slow');
+    $('body').removeClass('stop-scrolling');
   });
 
 });
