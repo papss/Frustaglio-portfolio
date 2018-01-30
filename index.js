6be5fe280992e85560f2c2ebb9091d6a8e7e0fed
Joseph Papaluca <jpapaluc@gmail.com>
2018-01-30 17:15:34 -0500

    bug update coming soon

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
 $(document).ready(function() {
 
-  // $.scrollify({
-  //   section: ".section",
-  //   offset: -150
-  // });
-
-  $('.test').on('click', function() {
-    $('.modal').fadeIn('slow');
-    $('body').addClass('stop-scrolling');
-  });
-
-  $('.modal').on('click', function() {
-    $('iframe').attr('src', $('iframe').attr('src'));
-    $('.modal').fadeOut('slow');
-    $('body').removeClass('stop-scrolling');
-  });
-
 });
