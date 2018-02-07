f280a272f02d72f5a1a593b02f7468646e9e5354
Joseph Papaluca <jpapaluc@gmail.com>
2018-02-07 18:45:36 -0500

    more sliders

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
 
+  $(".knob").click(function() {
+    if ( $(this).hasClass("knob-top") ) {
+      $(this).removeClass("knob-top");
+      $(this).addClass("knob-bottom");
+    } else if ( $(this).hasClass("knob-bottom") ) {
+      $(this).removeClass("knob-bottom");
+      $(this).addClass("knob-top");
+    }
+  });
+
 });
