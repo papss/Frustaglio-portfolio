a4a6f64d0d4a9d624097c319902f3f8135b8377e
Joseph Papaluca <jpapaluc@gmail.com>
2018-02-08 15:19:37 -0500

    sliders slide

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 $(document).ready(function() {
 
   $(".knob").click(function() {
+
     if ( $(this).hasClass("knob-top") ) {
+
       $(this).removeClass("knob-top");
+      $(this).animate( {top: '220px'}, 'slow' );
       $(this).addClass("knob-bottom");
+
     } else if ( $(this).hasClass("knob-bottom") ) {
+
+      $(this).animate( {top: '5px'}, 'slow' );
       $(this).removeClass("knob-bottom");
       $(this).addClass("knob-top");
+
     }
+
   });
 
 });
