0790e571df4935efe09bbca310fcb125fb5c5839
Joseph Papaluca <jpapaluc@gmail.com>
2018-02-09 12:06:37 -0500

    sliders are slidy-er

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 $(document).ready(function() {
 
-  $(".knob").click(function() {
+  var aboutKnob   = $(".about");
+  var portKnob    = $(".portfolio");
+  var contKnob    = $(".contact");
 
-    if ( $(this).hasClass("knob-top") ) {
+  // SLIDER GOES DOWN:
+  jQuery.fn.extend({
+    sliderDown: function () {
 
-      $(this).removeClass("knob-top");
-      $(this).animate( {top: '220px'}, 'slow' );
-      $(this).addClass("knob-bottom");
+      if ( $(this).hasClass("knob-top") ) {
+        $(this).removeClass("knob-top");
+        $(this).animate( {top: '220px'}, 'slow' );
+        $(this).addClass("knob-bottom");
 
-    } else if ( $(this).hasClass("knob-bottom") ) {
-
-      $(this).animate( {top: '5px'}, 'slow' );
-      $(this).removeClass("knob-bottom");
-      $(this).addClass("knob-top");
-
+      }
     }
-
   });
+
+  // SLIDER GOES UP: 
+  jQuery.fn.extend({
+    sliderUp: function () {
+
+      if ( $(this).hasClass("knob-bottom") ) {
+        $(this).animate( {top: '5px'}, 'slow' );
+        $(this).removeClass("knob-bottom");
+        $(this).addClass("knob-top");
+
+      }
+    }
+  });
+
+  $(".knob").click(function() {
+    $(aboutKnob).sliderDown();
+    $(portKnob).sliderDown();
+    $(contKnob).sliderDown();
+    $(this).sliderUp();
+  })
 
 });
